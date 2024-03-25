215fec34d5ed4313a6fba4ac62097df0fa18c2b4
Henry <hbreukelman20@gmail.com>
2024-03-25 14:48:57 -0500

    testing

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,26 +1,4 @@
 
-/*
-movies is now an array of movies
-
-function: searchMovies
-filter to compare the user input with the moview title
-return the movies found
-
-function listMovies(input)
-const ocurrences = searchMovies(input)
-if ocurrences.length > 0 list movies
-  interact through ocurrences and set movie titles to a <li>
-  <li> -> <ul>
-else -> movie not found
-
-function to get selected movie
-this function is like the first one, but instaed of includes() you just compare
-the movie titles with the imput (===)
-
-once the selected movie is found, you build the html and send it to its parent
-the css must be there already
-
-*/
 
 import { select } from './utils.js';
 'use strict';
@@ -73,4 +51,4 @@ function listMovies(input) {
   }
 }
 
- utils.listen('input', searchInput, () => listMovies(searchInput.value));
+utils.listen('input', searchInput, () => listMovies(searchInput.value));
